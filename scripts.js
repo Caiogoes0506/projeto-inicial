358cde90160afcce9f13f1be6943f29516b06611
caiogoes0506 <caiogoes0506@gmail.com>
2024-03-19 09:13:54 -0300

    formulario com cep

--- scripts.js
+++ scripts.js
@@ -10,10 +10,32 @@ var numero = document.getElementById("numero");
 var complemento = document.getElementById("complemento");
 var bairro = document.getElementById("bairro");
 var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
 function alertar(){
+    const url = `https://viacep.com.br/ws/${cep.value}/json`;
+    fetch(url)
+.then(resposta=>resposta.json())
+.then(data => {
+    logradouro.value = data.logradouro;
+    bairro.value = data.bairro;
+    cidade.value = data.cidade;
+    estado.value = data.uf;
+
+
     //alert(nome.value + " " + "clicou no botao!!");
-    saida.innerText = "nome: " + " " + nome.value + " \n E-mail: " + " " + email.value  + " \n telefone: " + " " + telefone.value + " \n cep: " + " " + cep.value + " \n logradouro: " + " " + logradouro.value+ " \n numero: " + " " + numero.value + " \n complemento: " + " " + complemento.value + " \n bairro: " + " " + bairro.value + " \n cidade: " + " " + cidade.value;
+    saida.innerText = "nome: " + " " + nome.value 
+    + " \n E-mail: " + " " + email.value  
+    + " \n telefone: " + " " + telefone.value 
+    + " \n cep: " + " " + cep.value 
+    + " \n logradouro: " + " " + logradouro.value
+    + " \n numero: " + " " + numero.value 
+    + " \n complemento: " + " " + complemento.value 
+    + " \n bairro: " + " " + bairro.value 
+    + " \n cidade: " + " " + cidade.value 
+    + " \n estado: " + " " + estado.value;
 
+// buscar o endereço pelo cep
 
-
+})
+.catch(error=>alert(error))
 }
